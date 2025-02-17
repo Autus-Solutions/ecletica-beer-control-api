18d4829153257479e3e56891d7fdcf9a288cb5c4
Victor Oliveira <victor.soliveira@ambevtech.com.br>
2025-02-17 14:32:11 -0300

    Adjusting value property to double

--- src/EcleticaBeerControl.Domain/Entities/Timeseries/Temperature.cs
+++ src/EcleticaBeerControl.Domain/Entities/Timeseries/Temperature.cs
@@ -4,6 +4,6 @@
     {
         public required DateTime Time { get; init; }
         public required string DeviceId { get; init; } = string.Empty;
-        public long? Value { get; init; }
+        public double? Value { get; init; }
     }
 }
--- src/EcleticaBeerControl.Worker/Payloads/Temperature.cs
+++ src/EcleticaBeerControl.Worker/Payloads/Temperature.cs
@@ -4,7 +4,7 @@ namespace EcleticaBeerControl.Worker.Payloads
 {
     public sealed record Temperature
     {
-        public long Degrees { get; set; }
+        public double Degrees { get; set; }
         public DegreeFormat DegreeFormat { get; set; }
     }
 }
