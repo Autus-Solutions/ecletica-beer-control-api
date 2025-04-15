2ae425255ff349a33e96ef11116f69db56d0fff2
Victor Oliveira <victor.soliveira@ambevtech.com.br>
2025-04-15 18:39:02 -0300

    Updating MCP Server

--- src/EcleticaBeerControl.Mcp/Program.cs
+++ src/EcleticaBeerControl.Mcp/Program.cs
@@ -15,12 +15,17 @@ try
 
     var builder = WebApplication.CreateBuilder(args);
 
+
     builder.Services
             .AddMcpServer()
             .WithToolsFromAssembly();
 
+    builder.Services.AddProblemDetails();
+
     var app = builder.Build();
+
     app.MapMcp();
+    app.UseExceptionHandler();
 
     await app.RunAsync();
     return 0;
--- src/EcleticaBeerControl.Mcp/Tools/UserTool.cs
+++ src/EcleticaBeerControl.Mcp/Tools/UserTool.cs
@@ -1,5 +1,4 @@
-﻿using EcleticaBeerControl.Domain.Interfaces;
-using EcleticaBeerControl.Domain.Models;
+﻿using EcleticaBeerControl.Domain.Models;
 using ModelContextProtocol.Protocol.Types;
 using ModelContextProtocol.Server;
 using System.ComponentModel;
