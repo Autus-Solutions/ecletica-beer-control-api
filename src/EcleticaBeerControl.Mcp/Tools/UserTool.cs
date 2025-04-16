842056d16ec1bca4da750f59941d00668ee5f8e4
Victor Oliveira <victor.soliveira@ambevtech.com.br>
2025-04-15 21:29:34 -0300

    Improving MCP Server configuration

--- src/EcleticaBeerControl.Mcp/Tools/UserTool.cs
+++ src/EcleticaBeerControl.Mcp/Tools/UserTool.cs
@@ -7,7 +7,7 @@ namespace EcleticaBeerControl.Mcp.Tools
     [McpServerToolType]
     public class UserTool
     {
-        [McpServerTool, Description("Retorna uma lista de usários da aplicação")]
+        [McpServerTool, Description("Retorna uma lista de todos os usuários")]
         public IList<User> GetUsers()
         {
             return new List<User>() {
@@ -18,13 +18,14 @@ namespace EcleticaBeerControl.Mcp.Tools
             };
         }
 
-        [McpServerTool, Description("Retorna usuários da aplicação pelo nome")]
-        public IList<User> GetUsersByName([Description("Nome do usuário")] string name)
+        [McpServerTool, Description("Retorna usuários pelo nome")]
+        public IList<User> GetUsersByName([Description("Nome do usuário")] string username)
         {
             return new List<User>() {
                 new User()
                 {
-                    Email = name
+                    UserName = username,
+                    Email = $"{username}@teste.com"
                 }
             };
         }
