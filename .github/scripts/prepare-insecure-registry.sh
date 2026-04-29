360df75cef677f054da6989934cb8362e646048c
Victor Oliveira <contato@victoroliveira.dev>
2026-04-28 23:38:25 -0300

    Fixing script host address

--- .github/scripts/prepare-insecure-registry.sh
+++ .github/scripts/prepare-insecure-registry.sh
@@ -1,7 +1,7 @@
 echo "Cleaning docker daemon.json ..."
 sudo rm -rf /etc/docker/daemon.json
 echo "Preparing ..."
-sudo bash -c 'echo "{\"insecure-registries\" : [\"5.161.202.205:32000\"]}" >> /etc/docker/daemon.json'
+sudo bash -c 'echo "{\"insecure-registries\" : [\"2.24.207.138:32000\"]}" >> /etc/docker/daemon.json'
 echo "Restarting docker ..."
 sudo systemctl restart docker
 echo "Finished!"
